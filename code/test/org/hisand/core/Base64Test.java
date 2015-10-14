4a2105cce279f97447af6c420c8bb888b6291cdf
Hisand Liu <hisand.liu@gmail.com>
2015-10-15 00:51:04 +0800

    m

--- code/test/org/hisand/core/Base64Test.java
+++ code/test/org/hisand/core/Base64Test.java
@@ -1,7 +1,13 @@
 package org.hisand.core;
 
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class Base64Test {
 	
@@ -20,5 +26,24 @@ public class Base64Test {
 		Assert.assertEquals(value, result2);
 		
 		System.out.println(value);
+	}
+	
+	@Test 
+	public void testMock() {
+	
+		//创建mock对象，参数可以是类，也可以是接口  
+        @SuppressWarnings("unchecked")
+		List<String> list = mock(List.class);  
+          
+        //设置方法的预期返回值  
+        when(list.get(0)).thenReturn("helloworld");  
+      
+        String result = list.get(0);  
+          
+        //验证方法调用(是否调用了get(0))  
+        verify(list).get(0);  
+          
+        //junit测试  
+        Assert.assertEquals("helloworld", result);
 	}
 }
