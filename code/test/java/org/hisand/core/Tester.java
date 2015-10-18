60b736ee2c97a242a2d5bb0dc8bb5572b34e70d9
Hisand Liu <hisand.liu@gmail.com>
2015-10-18 13:59:45 +0800

    m

--- code/test/java/org/hisand/core/Tester.java
+++ code/test/java/org/hisand/core/Tester.java
@@ -1,10 +1,26 @@
 package org.hisand.core;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.junit.Assert;
+
 import org.junit.Test;
 
 public class Tester {
 	@Test
-	public void test1() {
-		System.out.println("Hello World");
+	public void test1() throws Exception {
+		InputStream stream = Tester.class.getClassLoader().getResourceAsStream(
+				"data/readpuretext.txt");
+		BufferedReader reader = new BufferedReader(
+				new InputStreamReader(stream));
+		StringBuffer sb = new StringBuffer();
+		String temp = null;
+		while ((temp = reader.readLine()) != null) {
+			sb.append(temp);
+		}
+		reader.close();
+		Assert.assertEquals("测试读纯文本内容", sb.toString());
 	}
 }
